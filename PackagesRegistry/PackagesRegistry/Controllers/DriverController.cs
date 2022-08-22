be222c0080d42b06a4186e3b6afbed96e9a2f9cd
Francisco Mata <francisco.redondo23@gamil.com>
2022-08-22 14:59:12 -0600

    create driver done

---
+++ PackagesRegistry/PackagesRegistry/Controllers/DriverController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PackagesRegistry.Models;
+using PackagesRegistry.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackagesRegistry.Controllers
+{
+    public class DriverController : Controller
+    {
+        private readonly PackagesRegistryContext _context;
+        public DriverController(PackagesRegistryContext context) => _context = context;
+
+        public IActionResult Index()
+        {
+            ViewData["TransportCompanies"] = new SelectList(_context.TransportCompanies, "Id", "Name");
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(DriverViewModel driver)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            Driver driverEF = new Driver()
+            {
+                CompanyId = driver.CompanyId
+            };
+
+            _context.Add(driverEF);
+            _context.SaveChanges();
+            return View();            
+        }
+    }
+}
